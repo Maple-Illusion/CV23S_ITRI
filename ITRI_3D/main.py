06131fa53615176e7f3272e189fcdc55197aa757
ChrisHsiehTech <103394989+ChrisHsiehTech@users.noreply.github.com>
2023-06-01 23:32:53 +0800

    Update main.py

--- ITRI_3D/main.py
+++ ITRI_3D/main.py
@@ -1,70 +1,124 @@
 import argparse
 import pandas as pd
-import dataset.read_cam_params as cam
+import tool.read_cam_params as cam
 import inference.Segmentation as seg
 import inference.get_corners as gcor
-import dataset.cfg as cfg
 import cv2
 import os
 import numpy as np
 
 
-
-
-
 if __name__ == '__main__':
     paser = argparse.ArgumentParser(prog='main',description='Data preparation')
-    paser.add_argument('--camera_info',type=str,default=r'C:\Users\ataraxia\Documents\AV_LAB\NTU_CV\finals\ITRI_3D\ITRI_dataset\camera_info\lucid_cameras_x00\gige_100_fl_hdr_camera_info.yaml')
-    paser.add_argument('--cam_launch',type=str,default=r'C:\Users\ataraxia\Documents\AV_LAB\NTU_CV\finals\ITRI_3D\ITRI_dataset\camera_info\lucid_cameras_x00\camera_extrinsic_static_tf.launch')
-    paser.add_argument('--data_folder',type=str,default=r'C:\Users\ataraxia\Documents\AV_LAB\NTU_CV\finals\ITRI_3D\ITRI_dataset\seq1\dataset\1681710717_532211005')
-    paser.add_argument('--cam_type',type=str,default='fisheye')
+    paser.add_argument('--camera_info',type=str,default=r'D:\CV\ITRI_dataset\ITRI_dataset\camera_info\lucid_cameras_x00')
+    paser.add_argument('--cam_launch',type=str,default=r'D:\CV\ITRI_dataset\ITRI_dataset\camera_info\lucid_cameras_x00\camera_extrinsic_static_tf.launch')
+    paser.add_argument('--data_folder',type=str,default=r'D:\CV\ITRI_dataset\ITRI_dataset\seq2\dataset')
+    # paser.add_argument('--data_folder',type=str,default=r'D:\CV\ITRI_dataset\ITRI_dataset\seq2\dataset\1681116127_676490974')
     paser.add_argument('--cam_num',type=int,default=1)
+    paser.add_argument('--mask_folder',type=str,default=r'D:\CV\CV23S_ITRI-main_2\CV23S_ITRI-main\ITRI_3D')
+
     # paser.add_argument('--target',type=str,default=r'')
     args = paser.parse_args()
-    
+    dir_list = os.listdir(args.data_folder)
+    n = 0
+    fr_mask= np.loadtxt(os.path.join(args.mask_folder,'fr_mask.txt'), dtype=int)
+    fl_mask= np.loadtxt(os.path.join(args.mask_folder,'fl_mask.txt'), dtype=int)
+    f_mask= np.loadtxt(os.path.join(args.mask_folder,'f_mask.txt'), dtype=int)
+    b_mask= np.loadtxt(os.path.join(args.mask_folder,'b_mask.txt'), dtype=int)
 
-
+    for img_folder in dir_list:
     ################################### data preparation
-    
-    cam_root = args.camera_info
-    img_root = os.path.join(args.data_folder,'raw_image.jpg')
-    marker = pd.read_csv(os.path.join(args.data_folder,'detect_road_marker.csv'),header=None,encoding='utf-8')
-    
-    car_pos_matrix = pd.read_csv(os.path.join(args.data_folder,'initial_pose.csv'),header=None,encoding='utf-8')
-    img = cv2.imread(img_root,1)
-    h, w, c = img.shape
-    
-    cam_params = cam.read_camera_info(cam_root)
-    print(cam_params[0])
-    print(cam_params[1][0])
-    if args.cam_type == 'fisheye':
-        # new = np.zeros_like(img)
-        Knew = cam_params[0].copy()
-        Knew[(0,1), (0,1)] = 1 * Knew[(0,1), (0,1)]
-        new = cv2.fisheye.undistortImage(img,np.array(cam_params[0]),D=np.array(cam_params[1][0]) ,Knew=Knew)
-        # print('done')
-    # print(img.shape)
-    cv2.imshow('good',new)
-    cma_tf_ls = cfg.launch_file()
-    
+        img_root = os.path.join(args.data_folder,img_folder,'raw_image.jpg')
+        #img_root = r'D:\CV\ITRI_dataset\ITRI_dataset\seq2\dataset\1681116129_479546307\raw_image.jpg'
+        camara_type = pd.read_csv(os.path.join(args.data_folder,img_folder,'camera.csv'),header=None,encoding='utf-8')
+        camara_type = camara_type.iloc[0][0]
+        camara_type = camara_type.replace('/lucid_cameras_x00/', '')
+        camara_type = camara_type + '_camera_info.yaml'
+        cam_root = os.path.join(args.camera_info,camara_type)
 
-    # print(crop_img[0][:])
-    # print(h,w)
-    
-    cv2.waitKey(0)
-    ################################### Preprocess
+        # print(img_root)
+        #marker = pd.read_csv(r'D:\CV\ITRI_dataset\ITRI_dataset\seq2\dataset\1681116129_479546307\detect_road_marker.csv',header=None,encoding='utf-8')
 
-    ################################### Segmentation
-    crop_img, obj_id, prob, origin = seg.Crop(img,marker)
-    
+        marker = pd.read_csv(os.path.join(args.data_folder,img_folder,'detect_road_marker.csv'),header=None,encoding='utf-8')
+        marker = marker.astype('float')
+        marker = marker.clip(lower=0)
+
+        #marker.iloc[:,[3]]=marker.iloc[:,[3]].clip(0, 926)
+        # print(marker)
+        car_pos_matrix = pd .read_csv(os.path.join(args.data_folder,img_folder,'initial_pose.csv'),header=None,encoding='utf-8')
+        img = cv2.imread(img_root)
+        # print(img.shape)
+        
+        threshold = 0.3
+        ################################### Segmentation
+        crop_img, obj_id, prob, origin = seg.Crop(img,marker,threshold)
+        cma_params = cam.read_camera_info(cam_root)
+
+        ################################### Get corners
+        corner_imgs, imgs_corners= gcor.find_corner(crop_img,origin)  # imgs_corners 為四張圖f,fr,fl,b 的corner
+
+        # print(imgs_corners[0])
+        #imgs_corners[4]= imgs_corners[4].astype(int)
 
 
-    ################################### Get corners
-    img_corner = gcor.find_corner(crop_img)
-    
+        def setdiff2d_set(arr1, arr2):
+            set1 = set(map(tuple, arr1))
+            set2 = set(map(tuple, arr2))
+            return np.array(list(set1 - set2))
+        
+        #print(fr_mask)
+        # Convert the arrays to tuples
+        new_corners = []
 
-    ###########check and combine points
-    new = gcor.combine_crop(img,img_corner,origin)
 
-    ################################### Pinhole model
+
+        for i in range(len(imgs_corners)):
+            if camara_type == 'gige_100_fr_hdr_camera_info.yaml':
+                print(fr_mask.shape)
+                new_corner = gcor.mask_diff(imgs_corners[i],fr_mask)
+                new_corners.append(new_corner)
+
+            elif camara_type == 'gige_100_fl_hdr_camera_info.yaml':
+                new_corner = gcor.mask_diff(imgs_corners[i],fl_mask)
+                new_corners.append(new_corner)
+
+            elif camara_type == 'gige_100_f_hdr_camera_info.yaml':
+                new_corner = gcor.mask_diff(imgs_corners[i],f_mask)
+                new_corners.append(new_corner)
+
+            elif camara_type == 'gige_100_b_hdr_camera_info.yaml':
+                new_corner = gcor.mask_diff(imgs_corners[i],b_mask)
+                new_corners.append(new_corner)
+
+
+        # print(len(new_corners))
+        img2= cv2.imread(img_root)
+
+        point_size = 1
+        point_color = (0, 0, 255) # red
+        thickness = 3
+        
+        #print(asd)
+        for i in range(len(new_corners)):
+
+            for point in new_corners[i]:
+                x_l = point[0]
+                y_l = point[1]
+                #print(x_l,y_l)
+                cv2.circle(img2, (x_l,y_l), point_size, point_color, thickness)
+        cv2.imshow('test',img2)
+        cv2.waitKey(0)
+
+
+
+
+
+
+        ###########check and combine points
+        
+        # new = gcor.combine_crop(img,corner_imgs,origin)
+
+        ################################### Pinhole model
+
+
 
